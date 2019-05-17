d658183365c41fbace72d610dfeb7987b5af76a2
yuetang95 <tang.yue@husky.neu.edu>
2019-05-16 23:59:48 -0700

    small_project

---
+++ project/to-do-notes/App.js
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+import { Constants } from 'expo';
+
+import Main from "./app/component/Main";
+
+
+export default class App extends React.Component {
+  render() {
+    return (
+     <Main />
+    );
+  }
+}
+
---
+++ project/to-do-notes/README.md
@@ -0,0 +1,5 @@
+# TO DO list app
+
+## Overview
+This is a simple _TO-DO List App_ which use **React Native** as front-end.
+back-end service is TBC
